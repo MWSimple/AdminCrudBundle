b6f5443c2ec3b52d56c1f3eb898cf6c7ef5a473a
Gonzalo Alonso <gonkpo@gmail.com>
2015-06-24 15:20:05 -0300

    fix delete entity ACL

--- EventListener/ACLListener.php
+++ EventListener/ACLListener.php
@@ -18,12 +18,13 @@ class ACLListener
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
 
-        $aclConf = $this->container->parameters['mw_simple_admin_crud.acl'];
+        $aclConf = $this->container->hasParameter('mw_simple_admin_crud.acl') ?
+            $this->container->getParameter('mw_simple_admin_crud.acl') : null;
         if ($aclConf['use']) {
             if ($this->isInstanceOf($entity, $aclConf['entities'])) {
                 $aclManager = $this->container->get('mws_acl_manager');
                 //control ACL
-                $aclManager->controlACL($entity, 'DELETE');
+                $aclManager->controlACL($entity, 'DELETE', $aclConf['exclude_role']);
                 //elimino ACL
                 $aclManager->deleteACL($entity);
             }
