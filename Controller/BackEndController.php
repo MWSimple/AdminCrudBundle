02af0b1f6b1ca293083f5a777292dc45cdf8afa9
Gonzalo Alonso <gonkpo@gmail.com>
2015-07-03 19:53:29 -0300

    resolving #14

--- Controller/BackEndController.php
+++ Controller/BackEndController.php
@@ -3,7 +3,6 @@
 namespace MWSimple\Bundle\AdminCrudBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Yaml\Yaml;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
