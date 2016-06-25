a46dd7e8008fb458a73280bea0608dd576640924
Gonzalo Alonso <gonkpo@gmail.com>
2016-06-24 21:13:26 -0300

    reemplazando jquery boostrap

--- Command/MWSimpleCrudCommand.php
+++ Command/MWSimpleCrudCommand.php
@@ -69,80 +69,8 @@ class MWSimpleCrudCommand extends GenerateDoctrineCrudCommand
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         $questionHelper = $this->getQuestionHelper();
-        $questionHelper->writeSection($output, 'Welcome to the ADMIN CRUD generator');
+        $questionHelper->writeSection($output, 'Admin CRUD Generator');
 
-        // namespace
-        $output->writeln(array(
-            '',
-            'This command helps you generate ADMIN CRUD controllers and templates.',
-            '',
-            'First, you need to give the entity for which you want to generate a ADMIN CRUD.',
-            'You can give an entity that does not exist yet and the wizard will help',
-            'you defining it.',
-            '',
-            'You must use the shortcut notation like <comment>AcmeBlogBundle:Post</comment>.',
-            '',
-        ));
-
-        if ($input->hasArgument('entity') && $input->getArgument('entity') != '') {
-            $input->setOption('entity', $input->getArgument('entity'));
-        }
-
-        $question = new Question($questionHelper->getQuestion('The Entity shortcut name', $input->getOption('entity')), $input->getOption('entity'));
-        $question->setValidator(array('Sensio\Bundle\GeneratorBundle\Command\Validators', 'validateEntityName'));
-        
-        $autocompleter = new EntitiesAutoCompleter($this->getContainer()->get('doctrine')->getManager());
-        $autocompleteEntities = $autocompleter->getSuggestions();
-        $question->setAutocompleterValues($autocompleteEntities);
-        $entity = $questionHelper->ask($input, $output, $question);
-
-        $input->setOption('entity', $entity);
-        list($bundle, $entity) = $this->parseShortcutNotation($entity);
-
-        try {
-            $entityClass = $this->getContainer()->get('doctrine')->getAliasNamespace($bundle).'\\'.$entity;
-            $metadata = $this->getEntityMetadata($entityClass);
-        } catch (\Exception $e) {
-            throw new \RuntimeException(sprintf('Entity "%s" does not exist in the "%s" bundle. You may have mistyped the bundle name or maybe the entity doesn\'t exist yet (create it first with the "doctrine:generate:entity" command).', $entity, $bundle));
-        }
-
-        // write?
-        $withWrite = $input->getOption('with-write') ?: true;
-        $output->writeln(array(
-            '',
-            'By default, the generator creates actions: list, show, new, update, and delete.',
-        ));
-        // $withWrite = $dialog->askConfirmation($output, $dialog->getQuestion('Do you want to generate the "write" actions', $withWrite ? 'yes' : 'no', '?'), $withWrite);
-        $input->setOption('with-write', true);
-
-        // format
-        $format = $input->getOption('format');
-        $output->writeln(array(
-            '',
-            'The format to use for the generated CRUD is annotation.',
-        ));
-        // $format = $dialog->askAndValidate($output, $dialog->getQuestion('Configuration format (yml, xml, php, or annotation)', $format), array('Sensio\Bundle\GeneratorBundle\Command\Validators', 'validateFormat'), false, $format);
-        $input->setOption('format', 'annotation');
-
-        // route prefix
-        $prefix = $this->getRoutePrefix($input, $entity);
-        $output->writeln(array(
-            '',
-            'Determine the routes prefix (all the routes will be "mounted" under this',
-            'prefix: /prefix/, /prefix/new, ...).',
-            '',
-        ));
-        $prefix = $questionHelper->ask($input, $output, new Question($questionHelper->getQuestion('Routes prefix', '/'.$prefix), '/'.$prefix));
-        $input->setOption('route-prefix', $prefix);
-
-        // summary
-        $output->writeln(array(
-            '',
-            $this->getHelper('formatter')->formatBlock('Summary before generation', 'bg=blue;fg=white', true),
-            '',
-            sprintf('You are going to generate a CRUD controller for "<info>%s:%s</info>"', $bundle, $entity),
-            sprintf('using the "<info>%s</info>" format.', $format),
-            '',
-        ));
+        parent::interact($input, $output);
     }
 }
