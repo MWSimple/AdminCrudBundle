fa41dfa940d902a337aeae19ad87259ac26329ab
Gonzalo <gonkpo@gmail.com>
2016-07-11 15:56:30 -0300

    #58 busco config para poder editar y agregar el item

--- Command/MWSimpleMenuCommand.php
+++ Command/MWSimpleMenuCommand.php
@@ -2,21 +2,22 @@
 
 namespace MWSimple\Bundle\AdminCrudBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Yaml\Yaml;
 
-class MWSimpleMenuCommand extends Command
+class MWSimpleMenuCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
             ->setName('mwsimple:menu:additem')
             ->setAliases(array('mwsimple:menu:additem'))
-            ->setDescription('Agrega Item al Menu')
+            ->setDescription('Agrega Item al Menu, por el momento solamente configuracion en .yml')
             ->addArgument('name', InputArgument::REQUIRED, 'Nombre Label para el item por ejemplo: Admin')
             ->addArgument('url', InputArgument::REQUIRED, 'nombre de la ruta al index por ejemplo: admin_inicio')
             ->addArgument('config', InputArgument::OPTIONAL, 'path a config.yml por ejemplo: app/config/config.yml?')
@@ -25,14 +26,19 @@ class MWSimpleMenuCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->updateConfig($input, $output);
+    }
+
+    private function updateConfig(InputInterface $input, OutputInterface $output)
+    {
         $name = $input->getArgument('name');
         $url = $input->getArgument('url');
-        $config = $input->getArgument('config');
+        $configPath = $input->getArgument('config');
 
-        //$targetRoutingPath = $this->getContainer()->getParameter('kernel.root_dir').'/config/routing.yml';
-
-
-        $output->writeln($name);
+        $configs = Yaml::parse(file_get_contents($this->getContainer()->getParameter('kernel.root_dir').'/../'.$configPath));
+        foreach ($configs as $key => $value) {
+            $output->writeln($key);
+        }
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
@@ -40,12 +46,11 @@ class MWSimpleMenuCommand extends Command
         $helper = $this->getHelper('question');
         // name
         $name = $input->getArgument('name');
-        $output->writeln(array(
-            '',
-            '<info>Ingrese el Nombre o Label para el item</info>',
-            '',
-        ));
-        $question = new Question('[<comment>Admin</comment>]: ', $name);
+
+        $default = 'Admin';
+        $questionString = sprintf('<info>Ingrese el Nombre o Label para el item</info> [<comment>%s</comment>]', $default);
+        $question = new Question($questionString, $default);
+
         $question->setValidator(function ($value) {
             if (trim($value) == '') {
                 throw new \Exception('Ingrese un valor');
@@ -58,12 +63,11 @@ class MWSimpleMenuCommand extends Command
         $input->setArgument('name', $name);
         // url
         $url = $input->getArgument('url');
-        $output->writeln(array(
-            '',
-            '<info>Ingrese el Nombre de la ruta al action</info>',
-            '',
-        ));
-        $question = new Question('[<comment>admin_inicio</comment>]: ', $url);
+
+        $default = 'admin_inicio';
+        $questionString = sprintf('<info>Ingrese el Nombre de la ruta al action</info> [<comment>%s</comment>]', $default);
+        $question = new Question($questionString, $default);
+
         $question->setValidator(function ($value) {
             if (trim($value) == '') {
                 throw new \Exception('Ingrese un valor');
@@ -76,12 +80,11 @@ class MWSimpleMenuCommand extends Command
         $input->setArgument('url', $url);
         // config
         $config = $input->getArgument('config');
-        $output->writeln(array(
-            '',
-            '<info>Ingrese el path a config.yml</info>',
-            '',
-        ));
-        $question = new Question('Enter por defecto [<comment>app/config/config.yml</comment>]: ', $config);
+        
+        $default = 'app/config/config.yml';
+        $questionString = sprintf('<info>El path a config.yml donde se incluye mw_simple_admin_crud?</info> [<comment>%s</comment>]', $default);
+        $question = new Question($questionString, $default);
+
         $question->setValidator(function ($value) {
             if (trim($value) == '') {
                 throw new \Exception('Ingrese un valor');
